111dec4eecfc0b44e5d1bea75ff09988d05b4b86
Opemipo2000 <opsyok@gmail.com>
2023-09-11 13:32:08 +0100

    Added functionality, but with dlib installation errors

--- FaceRecognition/facerec/views.py
+++ FaceRecognition/facerec/views.py
@@ -7,6 +7,12 @@ import cv2
 import os
 import pickle
 from PIL import Image
+import dlib
+import imutils
+# for calculating dist b/w the eye landmarks
+from scipy.spatial import distance as dist
+# to get the landmark ids of the left and right eyes
+from imutils import face_utils
 
 # Remember to replace "from keras.engine.topology import get_source_inputs" to "from keras.utils.layer_utils import get_source_inputs" in the keras_vggface/models.py file.
 from keras_vggface.vggface import VGGFace
@@ -190,8 +196,92 @@ def identify_face(image,first_name,last_name):
 
     return identity
 
-register_details('./opemipo_registration.mp4',"Opemipo","Okunoren")
+def calculate_EAR(eye):
+  
+    # calculate the vertical distances
+    y1 = dist.euclidean(eye[1], eye[5])
+    y2 = dist.euclidean(eye[2], eye[4])
+  
+    # calculate the horizontal distance
+    x1 = dist.euclidean(eye[0], eye[3])
+  
+    # calculate the EAR
+    EAR = (y1+y2) / x1
+    return EAR
 
-test_image = cv2.imread('./opemipo_test.jpeg')
+def calculate_face_attentiveness(video_path):
+    # Variables
+    blink_thresh = 0.45
+    succ_frame = 2
+    count_frame = 0
+  
+    # Eye landmarks
+    (L_start, L_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (R_start, R_end) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']   
 
-print(identify_face(test_image,"Opemipo","Okunoren"))
+    # Load pre-trained face and eye detectors
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    while True:
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(
+            cv2.CAP_PROP_FRAME_COUNT):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+  
+        else:
+            _, frame = cap.read()
+            frame = imutils.resize(frame, width=640)
+
+            # Perform face detection
+            im_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = detector(im_gray)
+
+            for face in faces:
+    
+                # landmark detection
+                shape = predictor(im_gray, face)
+    
+                # converting the shape class directly
+                # to a list of (x,y) coordinates
+                shape = face_utils.shape_to_np(shape)
+    
+                # parsing the landmarks list to extract
+                # lefteye and righteye landmarks--#
+                lefteye = shape[L_start: L_end]
+                righteye = shape[R_start:R_end]
+    
+                # Calculate the EAR
+                left_EAR = calculate_EAR(lefteye)
+                right_EAR = calculate_EAR(righteye)
+    
+                # Avg of left and right eye EAR
+                avg = (left_EAR+right_EAR)/2
+                blink_count = 0
+                if avg < blink_thresh:
+                    count_frame += 1  # incrementing the frame count
+                else:
+                    if count_frame >= succ_frame:
+                        blink_count+=1
+                        cv2.putText(frame, 'Blink Detected:{}'.format(blink_count), (30, 30),
+                                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
+                    else:
+                        count_frame = 0
+    
+            cv2.imshow("Video", frame)
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
+    
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    video_path = '/Users/adetounokunoren/Downloads/attentiveness.mp4'
+    test_image = cv2.imread('./opemipo_test.jpeg')
+
+    register_details('./opemipo_registration.mp4',"Opemipo","Okunoren")
+    print(identify_face(test_image,"Opemipo","Okunoren"))   
+    calculate_face_attentiveness(video_path)
+
